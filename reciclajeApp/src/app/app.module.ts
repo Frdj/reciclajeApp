a706b3a53c6b89a81eeb6bbce8835fd8bc987a27
Santiago, Mamani <santiagojmamani@gmail.com>
2019-10-07 15:44:43 -0300

    Perfil de usuario creado y BottomNavbar modificado

--- reciclajeApp/src/app/app.module.ts
+++ reciclajeApp/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Angular Material
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { InformationComponent } from './pages/information/information.component';
 import { RecycleComponent } from './pages/recycle/recycle.component';
-import {MatDividerModule} from '@angular/material/divider';
 import { RetirarComponent } from './pages/retirar/retirar.component';
 import { OfrecerComponent } from './pages/ofrecer/ofrecer.component';
 import { SolicitudComponent } from './components/solicitud/solicitud.component';
@@ -28,12 +34,15 @@ import { SolicitudComponent } from './components/solicitud/solicitud.component';
     SolicitudComponent,
   ],
   imports: [
-    MatButtonModule,
-    MatDividerModule,
     BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
-    
+    MatButtonModule,
+    MatDividerModule,
+    MatCardModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- reciclajeApp/src/app/pages/retirar/retirar.component.ts
+++ reciclajeApp/src/app/pages/retirar/retirar.component.ts
@@ -7,181 +7,181 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RetirarComponent implements OnInit {
 
-listaRetirar = [{
-  nombre: 'Rodrigo Bueno', 
-  direccion: 'Lima 1003',
-  localidad: 'Ciudad Autonoma de Buenos aires',
-  materiales: [{
-    tipo: 'Cartón',
-    cantidad: 1,
-  },
-  {
-    tipo: 'Vidrio',
-    cantidad: 3,
-  },
-  {
-    tipo: 'Plastico',
-    cantidad: 2,
-  },
-  {
-    tipo: 'Papel',
-    cantidad: 1,
-  },
-]
-},{
-  nombre: 'Rodrigo Bueno', 
-  direccion: 'Lima 1003',
-  localidad: 'CABA',
-  materiales: [{
-    tipo: 'Cartón',
-    cantidad: 1,
-  },
-  {
-    tipo: 'Vidrio',
-    cantidad: 3,
-  },
-  {
-    tipo: 'Plastico',
-    cantidad: 2,
-  },
-  {
-    tipo: 'Papel',
-    cantidad: 1,
-  },
-]
-},{
-  nombre: 'Rodrigo Bueno', 
-  direccion: 'Lima 1003',
-  localidad: 'CABA',
-  materiales: [{
-    tipo: 'Cartón',
-    cantidad: 1,
-  },
-  {
-    tipo: 'Vidrio',
-    cantidad: 3,
-  },
-  {
-    tipo: 'Plastico',
-    cantidad: 2,
-  },
-  {
-    tipo: 'Papel',
-    cantidad: 1,
-  },
-]
-},{
-  nombre: 'Rodrigo Bueno', 
-  direccion: 'Lima 1003',
-  localidad: 'CABA',
-  materiales: [{
-    tipo: 'Cartón',
-    cantidad: 1,
-  },
-  {
-    tipo: 'Vidrio',
-    cantidad: 3,
-  },
-  {
-    tipo: 'Plastico',
-    cantidad: 2,
-  },
-  {
-    tipo: 'Papel',
-    cantidad: 1,
-  },
-]
-},{
-  nombre: 'Rodrigo Bueno', 
-  direccion: 'Lima 1003',
-  localidad: 'CABA',
-  materiales: [{
-    tipo: 'Cartón',
-    cantidad: 1,
-  },
-  {
-    tipo: 'Vidrio',
-    cantidad: 3,
-  },
-  {
-    tipo: 'Plastico',
-    cantidad: 2,
-  },
-  {
-    tipo: 'Papel',
-    cantidad: 1,
-  },
-]
-},{
-  nombre: 'Rodrigo Bueno', 
-  direccion: 'Lima 1003',
-  localidad: 'CABA',
-  materiales: [{
-    tipo: 'Cartón',
-    cantidad: 1,
-  },
-  {
-    tipo: 'Vidrio',
-    cantidad: 3,
-  },
-  {
-    tipo: 'Plastico',
-    cantidad: 2,
-  },
-  {
-    tipo: 'Papel',
-    cantidad: 1,
-  },
-]
-},{
-  nombre: 'Rodrigo Bueno', 
-  direccion: 'Lima 1003',
-  localidad: 'CABA',
-  materiales: [{
-    tipo: 'Cartón',
-    cantidad: 1,
-  },
-  {
-    tipo: 'Vidrio',
-    cantidad: 3,
-  },
-  {
-    tipo: 'Plastico',
-    cantidad: 2,
-  },
-  {
-    tipo: 'Papel',
-    cantidad: 1,
-  },
-]
-},{
-  nombre: 'Rodrigo Bueno', 
-  direccion: 'Lima 1003',
-  localidad: 'CABA',
-  materiales: [{
-    tipo: 'Cartón',
-    cantidad: 1,
-  },
-  {
-    tipo: 'Vidrio',
-    cantidad: 3,
-  },
-  {
-    tipo: 'Plastico',
-    cantidad: 2,
-  },
-  {
-    tipo: 'Papel',
-    cantidad: 1,
-  },
-]
-},]
+  listaRetirar = [{
+    nombre: 'Rodrigo Bueno',
+    direccion: 'Lima 1003',
+    localidad: 'Ciudad Autonoma de Buenos aires',
+    materiales: [{
+      tipo: 'Cartón',
+      cantidad: 1,
+    },
+    {
+      tipo: 'Vidrio',
+      cantidad: 3,
+    },
+    {
+      tipo: 'Plastico',
+      cantidad: 2,
+    },
+    {
+      tipo: 'Papel',
+      cantidad: 1,
+    },
+    ]
+  }, {
+    nombre: 'Rodrigo Bueno',
+    direccion: 'Lima 1003',
+    localidad: 'CABA',
+    materiales: [{
+      tipo: 'Cartón',
+      cantidad: 1,
+    },
+    {
+      tipo: 'Vidrio',
+      cantidad: 3,
+    },
+    {
+      tipo: 'Plastico',
+      cantidad: 2,
+    },
+    {
+      tipo: 'Papel',
+      cantidad: 1,
+    },
+    ]
+  }, {
+    nombre: 'Rodrigo Bueno',
+    direccion: 'Lima 1003',
+    localidad: 'CABA',
+    materiales: [{
+      tipo: 'Cartón',
+      cantidad: 1,
+    },
+    {
+      tipo: 'Vidrio',
+      cantidad: 3,
+    },
+    {
+      tipo: 'Plastico',
+      cantidad: 2,
+    },
+    {
+      tipo: 'Papel',
+      cantidad: 1,
+    },
+    ]
+  }, {
+    nombre: 'Rodrigo Bueno',
+    direccion: 'Lima 1003',
+    localidad: 'CABA',
+    materiales: [{
+      tipo: 'Cartón',
+      cantidad: 1,
+    },
+    {
+      tipo: 'Vidrio',
+      cantidad: 3,
+    },
+    {
+      tipo: 'Plastico',
+      cantidad: 2,
+    },
+    {
+      tipo: 'Papel',
+      cantidad: 1,
+    },
+    ]
+  }, {
+    nombre: 'Rodrigo Bueno',
+    direccion: 'Lima 1003',
+    localidad: 'CABA',
+    materiales: [{
+      tipo: 'Cartón',
+      cantidad: 1,
+    },
+    {
+      tipo: 'Vidrio',
+      cantidad: 3,
+    },
+    {
+      tipo: 'Plastico',
+      cantidad: 2,
+    },
+    {
+      tipo: 'Papel',
+      cantidad: 1,
+    },
+    ]
+  }, {
+    nombre: 'Rodrigo Bueno',
+    direccion: 'Lima 1003',
+    localidad: 'CABA',
+    materiales: [{
+      tipo: 'Cartón',
+      cantidad: 1,
+    },
+    {
+      tipo: 'Vidrio',
+      cantidad: 3,
+    },
+    {
+      tipo: 'Plastico',
+      cantidad: 2,
+    },
+    {
+      tipo: 'Papel',
+      cantidad: 1,
+    },
+    ]
+  }, {
+    nombre: 'Rodrigo Bueno',
+    direccion: 'Lima 1003',
+    localidad: 'CABA',
+    materiales: [{
+      tipo: 'Cartón',
+      cantidad: 1,
+    },
+    {
+      tipo: 'Vidrio',
+      cantidad: 3,
+    },
+    {
+      tipo: 'Plastico',
+      cantidad: 2,
+    },
+    {
+      tipo: 'Papel',
+      cantidad: 1,
+    },
+    ]
+  }, {
+    nombre: 'Rodrigo Bueno',
+    direccion: 'Lima 1003',
+    localidad: 'CABA',
+    materiales: [{
+      tipo: 'Cartón',
+      cantidad: 1,
+    },
+    {
+      tipo: 'Vidrio',
+      cantidad: 3,
+    },
+    {
+      tipo: 'Plastico',
+      cantidad: 2,
+    },
+    {
+      tipo: 'Papel',
+      cantidad: 1,
+    },
+    ]
+  },]
   constructor() {
     //ordena x localidad.
-   this.listaRetirar = this.listaRetirar.sort((a,b) => (a.localidad > b.localidad) ? 1 : -1);
-   }
+    this.listaRetirar = this.listaRetirar.sort((a, b) => (a.localidad > b.localidad) ? 1 : -1);
+  }
 
-   
+
   ngOnInit() {
   }
 
