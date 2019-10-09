82e96b2ea5b7c42fd18737ad2601bb1ba56481ec
Juan Cruz Von Elm <jvonelm@coelsa.com.ar>
2019-10-09 09:15:41 -0300

    Fix modelos

--- ReciclajeApi/ReciclajeApi/Models/Direccion.cs
+++ ReciclajeApi/ReciclajeApi/Models/Direccion.cs
@@ -1,8 +1,11 @@
 namespace ReciclajeApi{
     public class Direccion{
+        public int idUsuario {get; set;}
         public Usuario usuario {get; set;}
         public int nuDireccion {get; set;}
+        public int idProvincia {get; set;}
         public Provincia provincia {get; set;}
+        public int idLocalidad {get; set;}
         public Localidad localidad {get; set;}
         public string domicilio {get; set;}
     }
--- ReciclajeApi/ReciclajeApi/Models/Localidad.cs
+++ ReciclajeApi/ReciclajeApi/Models/Localidad.cs
@@ -1,5 +1,6 @@
 namespace ReciclajeApi{
     public class Localidad{
+        public int idProvincia {get; set;}
         public Provincia provincia {get; set;}
         public int idLocalidad {get; set;}
         public string descripcion {get; set;}
