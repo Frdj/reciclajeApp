35cc7257b1f2aaa88ca8dfee5a08061087512d88
AgasiD <agasidamian@gmail.com>
2019-10-31 23:22:36 -0300

    agregado pwa y solucionado el problema del material

--- reciclajeApp/src/app/models/Material.ts
+++ reciclajeApp/src/app/models/Material.ts
@@ -1,11 +1,8 @@
 export class Material{
-    
     tipo: string;
     cantidad: number;
-    
     constructor(tipo: string, cantidad: number){
         this.tipo = tipo;
         this.cantidad = cantidad;
-
     }
 }
--- reciclajeApp/src/app/pages/new-publish/new-publish.component.ts
+++ reciclajeApp/src/app/pages/new-publish/new-publish.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Material } from 'src/app/models/Material';
-import { InformationComponent } from '../information/information.component';
 import { InformationService } from '../../services/information.service';
 import { MescelaneasService } from '../../services/mescelaneas.service';
+import { Material } from '../../models/Material';
 
 @Component({
   selector: 'app-new-publish',
@@ -17,7 +16,7 @@ error = false;
 materiales: Material[] = [];
 
   constructor(private _informacion: InformationService, private misce: MescelaneasService) {
-    this._informacion.getMateriales('descripcion').subscribe((mats: any[]) =>{ mats.forEach(mat => {this.tipoMateriales.push(mat.descripcion)
+    this._informacion.getMateriales().subscribe((mats: any[]) =>{ mats.forEach(mat => {this.tipoMateriales.push(mat.descripcion)
     this.error = false; this.loading = false})
       this.AddElement();
     }, error => {misce.errorAlert(error); this.error = true; this.loading = false; })
