ff1b2ff49ee60933ac5fc3b7b9c0bef4203a5ccb
jcve <jcvonelm@gmail.com>
2019-11-07 18:33:07 -0300

    PublicacionApiModel

--- ReciclajeApi/ReciclajeApi/Business/Models/ApiModels/PublicacionApiModel.cs
+++ ReciclajeApi/ReciclajeApi/Business/Models/ApiModels/PublicacionApiModel.cs
@@ -4,30 +4,28 @@ namespace ReciclajeApi.Business.Models.ApiModels
 {
     public class PublicacionApiModel
     {
-        public int IdPublicacion { get; set; }
+        public int IdPublicacion {get; set;}
 
-        public UsuarioApiModel UsuarioP { get; set; }
+        public int IdUsuarioP {get; set;}
 
-        public DireccionApiModel Direccion { get; set; }
+        public int NuDireccion {get; set;}
 
-        public TipoResiduoApiModel TipoResiduo { get; set; }
+        public MaterialApiModel[] Residuos {get; set;}
 
-        public CategoriaResiduoApiModel CategoriaResiduo { get; set; }
+        public string Medida {get; set;}
 
-        public int Cantidad { get; set; }
+        public int IdDetalle {get; set;}
 
-        public string Medida { get; set; }
+        public DateTime FechaPublicacion {get; set;}
 
-        public DateTime FechaPublicacion { get; set; }
+        public string Estado {get; set;}
 
-        public EstadoApiModel Estado { get; set; }
+        public string DiasDisponibles {get; set;}
 
-        public string DiasDisponibles { get; set; }
+        public string HorarioDisponible {get; set;}
 
-        public string HorarioDisponible { get; set; }
+        public int UsuarioR {get; set;}
 
-        public UsuarioApiModel UsuarioR { get; set; }
-
-        public DateTime? FechaRetiro { get; set; }
+        public DateTime FechaRetiro {get; set;}
     }
 }
